0e34047f56381f1d2e88710addbfaa4c4e8d9645
thevalenciandev <thevalenciandev@gmail.com>
2019-08-23 20:21:32 +0100

    Unit Testing BeerService (no Spring involved in this one): testing Id not found

--- beertracker-server/src/test/java/com/thevalenciandev/beertracker/service/BeerServiceTest.java
+++ beertracker-server/src/test/java/com/thevalenciandev/beertracker/service/BeerServiceTest.java
@@ -1,5 +1,6 @@
 package com.thevalenciandev.beertracker.service;
 
+import com.thevalenciandev.beertracker.controller.BeerNotFoundException;
 import com.thevalenciandev.beertracker.domain.Beer;
 import com.thevalenciandev.beertracker.repository.BeerRepository;
 import org.junit.Before;
@@ -11,6 +12,7 @@ import org.mockito.junit.MockitoJUnitRunner;
 import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.catchThrowable;
 import static org.mockito.BDDMockito.given;
 
 @RunWith(MockitoJUnitRunner.class)
@@ -35,5 +37,14 @@ public class BeerServiceTest {
         assertThat(beer.getName()).isEqualTo("Innovation IPA");
         assertThat(beer.getType()).isEqualTo("IPA");
         assertThat(beer.getABV()).isEqualTo(6.7);
+    }
+
+    @Test
+    public void throwsExceptionUponUnknownId() {
+        given(beerRepository.findById(666L)).willThrow(BeerNotFoundException.class);
+
+        Throwable thrown = catchThrowable(() -> beerService.getBeerDetails(666L));
+
+        assertThat(thrown).isInstanceOf(BeerNotFoundException.class);
     }
 }
