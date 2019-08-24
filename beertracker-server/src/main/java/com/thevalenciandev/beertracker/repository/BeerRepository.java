ccc066202cb084d87868076e1905076bb0996b9a
thevalenciandev <thevalenciandev@gmail.com>
2019-08-24 15:10:49 +0100

    Adding findByName capability (comes for free by reflection when following the findByXXX pattern because it matches the property "name"). Refactoring test.

--- beertracker-server/src/main/java/com/thevalenciandev/beertracker/repository/BeerRepository.java
+++ beertracker-server/src/main/java/com/thevalenciandev/beertracker/repository/BeerRepository.java
@@ -3,6 +3,9 @@ package com.thevalenciandev.beertracker.repository;
 import com.thevalenciandev.beertracker.domain.Beer;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.Optional;
+
 public interface BeerRepository extends CrudRepository<Beer, Long> {
 
+    Optional<Beer> findByName(String name);
 }
--- beertracker-server/src/test/java/com/thevalenciandev/beertracker/repository/BeerRepositoryTest.java
+++ beertracker-server/src/test/java/com/thevalenciandev/beertracker/repository/BeerRepositoryTest.java
@@ -6,12 +6,14 @@ import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
+import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 @RunWith(SpringRunner.class)
 @DataJpaTest
+@DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_EACH_TEST_METHOD) // clean DB after each test
 public class BeerRepositoryTest {
 
     @Autowired
@@ -25,8 +27,26 @@ public class BeerRepositoryTest {
 
         Beer savedBeer = entityManager.persistFlushFind(new Beer("Innovation IPA", "IPA", 6.7));
 
-        Beer beer = repository.findById(1L).get();
+        Beer retrievedBeer = repository.findById(1L).get();
 
+        assertEquals(savedBeer, retrievedBeer);
+    }
+
+    @Test
+    public void canRetrieveBeerByName() {
+
+        Beer savedBeer = entityManager.persistFlushFind(beerOfName("Innovation IPA"));
+
+        Beer retrievedBeer = repository.findByName("Innovation IPA").get();
+
+        assertEquals(savedBeer, retrievedBeer);
+    }
+
+    private Beer beerOfName(String name) {
+        return new Beer(name, "a-type", 12.3);
+    }
+
+    private void assertEquals(Beer savedBeer, Beer beer) {
         assertThat(beer.getName()).isEqualTo(savedBeer.getName());
         assertThat(beer.getType()).isEqualTo(savedBeer.getType());
         assertThat(beer.getABV()).isEqualTo(savedBeer.getABV());
